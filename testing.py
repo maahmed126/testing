25d39544506d9a50828b1c8e58f50bf2ed79c1aa
Md Ahmed <maahmed126@gmail.com>
2021-11-24 16:03:33 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
+def handler(event,context):
 
+    kg=event['queryStringParameters']['kg']
+    lb=float(kg)*2.20462
+    ans = str(kg) + " Kgs is " + str(round(lb, 2)) + " lbs\n"
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': ans
+    }
